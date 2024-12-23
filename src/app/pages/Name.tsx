822d91e8298e427b7fddd37a417d7a068af74233
syedtechnical <muqtadeer960@gmail.com>
2024-12-23 21:56:58 +0530

    under contruction

--- src/app/pages/Name.tsx
+++ src/app/pages/Name.tsx
@@ -1,12 +1,48 @@
 import { Box, Container, Stack, Text } from "@chakra-ui/react";
 import React from "react";
-import "@/app/styles/main.css";
+import { FcProcess } from "react-icons/fc";
 const Name = () => {
   return (
     <>
       <Box pt={{ base: 5, sm: 10, md: 12 }}>
         <Container>
           <Stack>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "right",
+              }}
+            >
+              <small
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                  gap: "8px",
+                  fontSize: "16px",
+                  fontWeight: "bold",
+                  animation: "colorZoom 3s infinite",
+                  color: "#ff6a00",
+                }}
+              >
+                <FcProcess /> Site under construction
+              </small>
+
+              <style>
+                {`
+          @keyframes colorZoom {
+            0%, 100% {
+              transform: scale(1);
+              color: #ff6a00;
+            }
+            50% {
+              transform: scale(1.1);
+              color: #ee0979;
+            }
+          }
+        `}
+              </style>
+            </div>
             <Text
               fontWeight={"bold"}
               textStyle={{ base: "4xl", sm: "5xl", md: "6xl" }}
--- src/app/types/typewriter-effect.d.ts
+++ src/app/types/typewriter-effect.d.ts
@@ -2,3 +2,8 @@ declare module "typewriter-effect/dist/core" {
   const Typewriter: any;
   export default Typewriter;
 }
+
+declare module "responsive-navbar-react" {
+  const Navbar: any;
+  export { Navbar };
+}
